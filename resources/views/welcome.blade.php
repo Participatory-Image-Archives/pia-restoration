f1cd61ecb4fc2b2c3a2f11e8a6b0c0981810dac0
Adrian Demleitner <adrian@thgie.ch>
2022-03-10 15:38:11 +0100

    changes to index view with UI and added search

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,16 @@
 </div>
 
 <div class="md:flex">
-    <div class="w-full md:w-1/3">
-        <h2 class="text-2xl mb-4">Bisherige Notizen</h2>
-        <ul class="">
+    <div id="sets-list" class="w-full md:w-1/3">
+        <h2 class="text-2xl mb-2">Bisherige Notizen</h2>
+        <input type="text" class="fuzzy-search mb-4 border-b border-gray-600" placeholder="Notizen durchsuchen">
+        <ul class="list">
         @foreach ($sets_chron as $set)
             @if ($set->label)
                 <li class="mb-2">
                     &mdash; <span>
-                        <a href="{{ route('sets.edit', [$set]) }}" class="pl-1 font-bold hover:underline">{{ date('d. M Y - H:i', strtotime($set->created_at)) }}</a><br>
-                        <span class="ml-6 block text-xs break-all leading-relaxed">{{ $set->signatures }}</span>
+                        <a href="{{ route('sets.edit', [$set]) }}" class="signatures pl-1 font-bold hover:underline">{{ $set->signatures }}</a><br>
+                        <span class="date ml-6 block text-xs break-all leading-relaxed">{{ date('d. M Y - H:i', strtotime($set->created_at)) }}</span>
                     </span>
                 </li>
             @endif
@@ -26,12 +27,12 @@
 @endsection
 
 @section('scripts')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/1.0.2/list.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
     <script>
 
         document.addEventListener('DOMContentLoaded', () => {
-            var searchable_list = new List('searchable-list', {
-                valueNames: ['label']
+            var searchable_list = new List('sets-list', {
+                valueNames: ['signatures', 'date']
             });
         });
 
